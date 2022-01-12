8b8d97acc52a87dd4e76e9665ea2a64f071bf881
Riudiu <lacvert13@gmail.com>
2022-01-12 14:51:55 +0900

    State 관리 - 반응형 상태 관리

--- lib/src/page/ReactiveStateManage.dart
+++ lib/src/page/ReactiveStateManage.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sample_get_x/src/page/controller/CountControllerWithReactive.dart';
 
 class ReactiveStateManage extends StatelessWidget {
   const ReactiveStateManage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Get.put(CountControllerWithReactive());
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Reactive State Manage Page'),
-        backgroundColor: Colors.pinkAccent,
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Reactive State Manage Page'),
+          backgroundColor: Colors.pinkAccent,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("GetX", style: TextStyle(fontSize: 30)),
+              // Obx()의 경우 사용할 컨트롤러의 종류를 따로 명시할 필요가 없고, 보여줄 위젯만 리턴하면 된다.
+              // 하지만 이 방법은 무조건 Get.put()을 필요로 한다.
+
+              // 여기서 Obx는 count의 value 값이 변경될 때만 호출된다.
+              // 만약에 count가 1일 때 버튼을 클릭해서 5로 변경시키면 Obx가 호출된다.
+              // 그러나 5인 상태에서 5로 변경하는 버튼을 클릭하면 호출되지 않는다. count의 값은 똑같이 5이기 때문이다.
+              // 따라서 반응형의 경우 비교적 리소스를 덜 사용하면서 화면을 제어할 수 있게 된다.
+              Obx(() => Text(
+                  "${Get.find<CountControllerWithReactive>().count.value}",
+                  style: TextStyle(fontSize: 50))),
+              SizedBox(height: 15),
+              ElevatedButton(
+                child: Text("+", style: TextStyle(fontSize: 30)),
+                onPressed: () {
+                  Get.find<CountControllerWithReactive>().increase();
+                },
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                child: Text("5로 변경", style: TextStyle(fontSize: 20)),
+                onPressed: () {
+                  Get.find<CountControllerWithReactive>().putNumber(5);
+                },
+              ),
+            ],
+          ),
+        ));
   }
 }
---
+++ lib/src/page/controller/CountControllerWithReactive.dart
@@ -0,0 +1,58 @@
+import 'package:get/get.dart';
+
+enum NUM { FIRST, SECOND }
+
+class User {
+  String name = "";
+  int age = 0;
+
+  User({required this.name, required this.age});
+}
+
+// getX는 컨트롤러의 생성과 삭제를 알아서 해준다.
+// instance했다는 것은 메모리에 올라가는 것인데 getX는 컨트롤러의 instance를 알아서 삭제해서 메모리를 관리해준다.
+class CountControllerWithReactive extends GetxController {
+  // obs는 observable의 약자
+  // 반응형으로 obs가 변화를 감지해준다.
+  RxInt count = 0.obs;
+  // 다른 타입들도 가능
+  RxDouble doubleValue = 0.0.obs;
+  RxString stringValue = "".obs;
+  Rx<NUM> num = NUM.FIRST.obs;
+  Rx<User> user = User(name: "", age: 0).obs;
+  RxList<String> list = <String>[].obs;
+  RxList<dynamic> list22 = [].obs;
+
+  void increase() {
+    count++;
+  }
+
+  void putNumber(int value) {
+    count(value);
+  }
+
+  // onInit같은 생명주기는 GetxController를 상속받아야 사용할 수 있다.
+  // 하지만 기본적은 GetX의 reactive 기능은 GetxController를 상속받지 않아도 사용 가능하다.
+  @override
+  void onInit() {
+    // reactive 방식에서만 사용할 수 있는 특별한 기능들이 바로 workers이다.
+    // 이를 사용하면 Rx 변수들의 변화를 감지하고 다양한 상황 별로 적절한 대응을 할 수 있다.
+
+    // count가 처음으로 변경되었을 때만 호출된다.
+    once(count, (_) => print("처음 한 번만 호출"));
+
+    // count가 변경될 때마다 호출된다.
+    ever(count, (_) => print("매번 호출"));
+
+    // count가 변경되다가 마지막 변경 후, duration 값 동안(여기선 2초간) 변경이 없을 때 한번 호출된다.
+    // debounce는 검색 기능 같은 데에서 쓸 수 있다. 최종 입력을 멈췄을 때 api 호출
+    debounce(count, (_) => print("마지막 변경 후에 한 번만 호출"),
+        time: Duration(seconds: 2));
+
+    // count가 변경되고 있는 동안, duration 값의 간격마다 호출된다.
+    interval(count, (_) => print("변경되고 있는 동안 1초마다 호출"),
+        time: Duration(seconds: 1));
+
+    super.onInit();
+  }
+}
